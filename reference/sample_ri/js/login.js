0158db5b1935e5361d6f930b02b287eeac15677d
b-naresh <naresh.baleswaran@gmail.com>
2014-06-24 15:43:26 +0530

    Initial Commit
    Initial Commit to GITHUB

---
+++ reference/sample_ri/js/login.js
@@ -0,0 +1,9 @@
+var xwtLoginData = {
+	    "applicationName": "Cisco Prime Device Configuration",
+            "loginActionUrl": "index.html",
+	    "versionNumber": "Version 1.0",
+	    "applicatonImage":"../sample_ri/images/login_sprite.png",
+	    "copyrightInfo": "@ 2014 Cisco Systems, Inc. Cisco, Cisco Systems and Cisco Systems logo are registered trademarks of Cisco Systems, Inc. and/or its affiliates in the U.S. and certain other countries"
+}
+    
+new gXwt_lite.login().init();
